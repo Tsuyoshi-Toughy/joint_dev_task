e43f6a96212390708ad968b2eca8547f235ff089
Tsuyoshi-Toughy <ogutsuyoshi@gmail.com>
2020-10-28 10:49:10 +0900

    add Q1-Q5 answer

--- task.rb
+++ task.rb
@@ -2,9 +2,11 @@
 # 回答の出力を確認される際は，「ruby main.rb」をターミナルから実行して下さい。
 
 def q1
-  names = ["田中", "佐藤", "佐々木", "高橋"]
+  names = ["田中", "佐藤", "佐々木", "高橋",]
 
   # 以下に回答を記載
+  names.push("斎藤")
+  p names
 
 end
 
@@ -13,13 +15,15 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  p array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
 
 end
 
@@ -27,6 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
 
   # 以下は変更しないで下さい
   p sports
@@ -37,7 +42,9 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  [array1,array2].each do |array|
+    p array.empty?
+  end
 end
 
 def q6
